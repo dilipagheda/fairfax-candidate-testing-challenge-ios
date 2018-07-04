d73bb6d69b301315aaecba1d1401fa55d761c228
DILIP <dilip_agheda@yahoo.com>
2018-07-04 22:14:57 +1000

    Final refinement. added expectation and timeouts so that it will cater for delays up to 30 secs

--- FFTestUITests/FFTestUITests.swift
+++ FFTestUITests/FFTestUITests.swift
@@ -8,7 +8,7 @@ import XCTest
 class FFTestUITests: XCTestCase {
     
     let app = XCUIApplication()
-    
+    let serviceTimeout = 30
     override func setUp() {
         super.setUp()
         
@@ -37,8 +37,17 @@ class FFTestUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
         let mainTableView = app.tables["newsTableView"]
+        let exists = NSPredicate(format: "exists == true")
+        
+        expectation(for: exists, evaluatedWith: mainTableView, handler: nil)
+        waitForExpectations(timeout: TimeInterval(serviceTimeout), handler: nil)
+      
         XCTAssert(mainTableView.exists,"main table view containing news cells doesn't exist")
+        
         let newsCellAtIndex0 = mainTableView.cells["NewsCell0"]
+        expectation(for: exists, evaluatedWith: newsCellAtIndex0, handler: nil)
+        waitForExpectations(timeout: TimeInterval(serviceTimeout), handler: nil)
+        
         XCTAssert(newsCellAtIndex0.staticTexts["headline"].exists, "Headline for first news cell doesn't exist")
         XCTAssert(newsCellAtIndex0.staticTexts["abstract"].exists, "Abstract for first news cell doesn't exist")
         XCTAssert(newsCellAtIndex0.staticTexts["byLine"].exists, "by line for first news cell doesn't exist")
@@ -49,16 +58,24 @@ class FFTestUITests: XCTestCase {
     //Verify, index screen has no more than 10 articles even when service has returned more than 10. at the moment, given endpoint seems to be returning 16 but index screen only shows 10 as expected.
     //Below test will continue swiping up as long as new cells are found. after multiple swiping, if no new cell is found then loop will exit and test will check total number of cells. if they exceed 10 then test fails
     func testIndexScreenHasNoMoreThan10Articles(){
-        sleep(7)
-        let mainTableView = app.tables["newsTableView"]
-        XCTAssert(mainTableView.exists,"main table view containing news cells doesn't exist")
-        sleep(2)
         
+        let mainTableView = app.tables["newsTableView"]
+        let exists = NSPredicate(format: "exists == true")
+        
+        expectation(for: exists, evaluatedWith: mainTableView, handler: nil)
+        waitForExpectations(timeout: TimeInterval(serviceTimeout), handler: nil)
+        
+        XCTAssert(mainTableView.exists,"main table view containing news cells doesn't exist")
+        
+        //wait for first new item to load
+        expectation(for: exists, evaluatedWith: mainTableView.cells["NewsCell0"], handler: nil)
+        waitForExpectations(timeout: TimeInterval(serviceTimeout), handler: nil)
         
         var i: Int = 0
         var isFinish : Bool = false
         var ValueOfiBeforeSwipe: Int = 0
         repeat {
+            
             if mainTableView.cells["NewsCell\(i)"].exists {
                 i+=1
             }else {
@@ -81,9 +98,14 @@ class FFTestUITests: XCTestCase {
     //Verify, tapping a story should take user to full article
     func testGoToFullArticle() {
         let mainTableView = app.tables["newsTableView"]
+        let exists = NSPredicate(format: "exists == true")
+        expectation(for: exists, evaluatedWith: mainTableView, handler: nil)
+        waitForExpectations(timeout: TimeInterval(serviceTimeout), handler: nil)
+        
         XCTAssert(mainTableView.exists,"main table view containing news cells doesn't exist")
         let newsCellAtIndex0 = mainTableView.cells["NewsCell0"]
-        
+        expectation(for: exists, evaluatedWith: newsCellAtIndex0, handler: nil)
+        waitForExpectations(timeout: TimeInterval(serviceTimeout), handler: nil)
         //tap on the first article
         newsCellAtIndex0.tap()
 
@@ -98,7 +120,4 @@ class FFTestUITests: XCTestCase {
         
     }
 
-    //Verify swipeup and swipedown operations. check app doesn't crash
-    
-    //Verify latest article comes first based on timestamp
 }
